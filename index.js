470225a35fd4195a268a29e5b65889ba75833aad
Agoha Wisdom <117744262+Mawiztech@users.noreply.github.com>
2024-07-24 18:15:25 +0100

    Add files via upload
    javascript is fun

---
+++ index.js
@@ -0,0 +1,39 @@
+
+//Strings
+var person ="Maduka"
+var school="Excellent Schools"
+var church = "Watchman"
+var food = "rice"
+var color ="black"
+
+//Numbers
+
+let time = 56
+let money = 567
+let hour = 400
+let years = 78
+let tenure = 8
+
+//Booleans
+
+let available = true
+let finish = false
+let EndNow = true
+let OnTime = true
+let score = false
+
+//Null
+
+var confirm = null
+var accept = null
+var talk = null
+var teacher = null
+var fake = null
+
+//Undefined
+
+var student = undefined
+var grade = undefined
+var home = undefined
+var phone = undefined
+var jam = undefined
